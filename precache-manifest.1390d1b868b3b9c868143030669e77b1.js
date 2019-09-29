c8294d8c28ebca60c06ccb233aaef8b6fb1cef87
Lasha Goshadze <lgoshadze@productsavvy.com>
2019-09-29 23:20:03 +0300

    Updates

---
+++ precache-manifest.1390d1b868b3b9c868143030669e77b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f76b607dc26b6e3e1c1131da40d7e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5023ba50c28bf1f9624e",
+    "url": "/static/css/main.2eda3f22.chunk.css"
+  },
+  {
+    "revision": "13cb280c274b2df49224",
+    "url": "/static/js/2.ebe9b0c6.chunk.js"
+  },
+  {
+    "revision": "5023ba50c28bf1f9624e",
+    "url": "/static/js/main.516b3073.chunk.js"
+  },
+  {
+    "revision": "48faf6ed4557e06db8a8",
+    "url": "/static/js/runtime-main.0dfc9b0e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
