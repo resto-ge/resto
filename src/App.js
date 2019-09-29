028a1e1482df77acd82ba1106f0fed6faa29b67b
Lasha Goshadze <lgoshadze@productsavvy.com>
2019-09-29 21:36:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function FunctionalCounter() {
+  const [value, setValue] = React.useState(0);
+  return (<div>
+    <span>გაიგე რამდენად ყლე ხარ: {value}  </span>
+    <button style={{backgroundColor:'black', color:'white', height:30}} onClick={() => setValue(oldValue => oldValue + 1)}>დააჭირე</button>
+  </div>);
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Resto
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="/#"
           rel="noopener noreferrer"
         >
-          Learn React
+          Resto.ge © 2019
         </a>
+        <br />
+        <FunctionalCounter />
       </header>
     </div>
   );
